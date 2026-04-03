403d90d31ce9462cac158bfea02442a63d1fe587
tanya <tanya23mv@gmail.com>
2026-04-03 20:58:52 +0530

    Initial commit - added navbar with conditional rendering and JSON server setup

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,5 @@
+import styles from "./login.module.css"
+
+export default function Login(){
+    return <div className={styles.login}>Login Page</div>;
+}
---
+++ app/contact/edit/[id]/page.tsx
@@ -0,0 +1,5 @@
+
+export default function EditContactPage(){
+    return <div>Edit Contact Page</div>;
+
+};
